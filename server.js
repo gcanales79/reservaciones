113c1adeba0e729b28139a8dbde1a0628fe1506f
Gustavo Canales <gustavo.canales@me.com>
2019-01-19 12:39:24 -0600

    added CORS to server

--- server.js
+++ server.js
@@ -110,6 +110,15 @@ app.use(function (req, res, next) {
   next();
 });
 
+// CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-token,");
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  next();
+});
+// END CORS
+
 
 // Routes
 require("./routes/apiRoutes")(app);
